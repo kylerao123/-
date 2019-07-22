c72ccbee0b465ed37a7ca2b7b9b0178b39ae94f1
kylerao123 <1198992680@qq.com>
2019-07-22 19:24:56 +0800

    1

--- 发邮件.py
+++ 发邮件.py
@@ -1,11 +1,12 @@
 import pymysql
 import pymysql.cursors
 import json
+import time
 
 # 插入数据库
 def insertdata(x):
 	insert='INSERT INTO mails (account,id,title,sender,content,\
-		attachment,is_read,is_hold,read_delete,creat_time,due_time)\
+		attachment,is_read,is_hold,read_delete,create_time,due_time)\
 		 VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
 	logic_cursor.execute(insert,x)
 	logic.commit()
@@ -15,9 +16,10 @@ def id():
 	idlis='select id from mails where account=%s'%player_name
 	logic_cursor.execute(idlis)
 	idnums=logic_cursor.fetchall()
-	if idnums==[]:
+	if idnums==():
 		return 0
 	else:
+		# return idnums
 		return idnums[-1][0]+1
 
 
@@ -38,13 +40,19 @@ logic_cursor = logic.cursor()
 
 player_name=input('输入角色账号：')
 
+timestamp = time.time()+1800
+timeArray=time.localtime(timestamp)
+formatTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+
 while True:
 	name_num=input('输入物品ID和数量：')
 	num=int(name_num.split(' ')[-1])
-	lis=map(int,name_num.split(' '))
+	# print(id())
+	lis=list(map(int,name_num.split(' ')))
 	for x in lis[:-1]:
 		value=['%s'%player_name,'%s'%id(),'testmail','admin','福利',\
-		'%s'%attachment(x),'0','0','1','','']
+		'%s'%attachment(x),'0','0','1','','%s'%formatTime]
+		
 		insertdata(value)
 	print('数据插入成功')
 	print(' ')
